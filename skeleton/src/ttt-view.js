acfcfffe934911fda96c19ca0a4dd2052e6f30a3
akerpelm <akerpelm@u.rochester.edu>
2021-04-21 14:52:34 -0400

    Implement working grid and initial bindEvents functionality

--- skeleton/src/ttt-view.js
+++ skeleton/src/ttt-view.js
@@ -1,20 +1,57 @@
 class View {
-  constructor(game, $el) {}
+  constructor(game, $el) {
+    this.game = game;
+    this.$el = $el;
 
-  bindEvents() {}
+    this.setupBoard();
+  }
 
-  makeMove($square) {}
+  bindEvents() {
+    // const $li = $('li');
+    // const $ul = $('ul');
+    // const toggleClass = (e) => {
+    //   $li.toggleClass('X O');
+    //   this.game.playMove([1,1]);
+    // }
+    this.$el.on('click', 'li', (e) => {
+      $square = $(e.target);
+
+      this.makeMove($square);
+    }) //determine what/how to access square
+
+
+  }
+
+  makeMove($square) {
+    const pos = $square.data('pos');
+    const currentPlayer = this.game.currentPlayer;
+
+    $square.addClass(currentPlayer)
+    try {
+      this.game.playMove(pos);
+    } catch {
+      alert('Invalid move!');
+      return;
+    }
+
+    // if (this.game.currentPlayer === 'X') {
+    //   $square.text('X');
+    // } else {
+    //   $square.text('O');
+    // }
+  }
 
   setupBoard() {
     const $ul = $('<ul>');
 
     for(let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
-        const $li = $('<li>');
+        let $li = $('<li>');
         $ul.append($li);
+        $li.data('pos', [i, j])
       }
-      
     }
+    return this.$el.append($ul);
   }
 }
 
