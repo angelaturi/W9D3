76eae0fc013a09cca3b4827b62902143920e289a
aturi <angelaturi99@gmail.com>
2021-04-21 16:20:52 -0400

    Start jQuery Towers of Hanoi

---
+++ skeleton_2/src/index.js
@@ -0,0 +1,10 @@
+const HanoiGame = require('./game.js')
+const HanoiView = require('./hanoi-view.js')
+
+// console.log("it works!")
+
+// $(() => {
+//   const rootEl = $('.hanoi');
+//   const game = new HanoiGame();
+//   new HanoiView(game, rootEl);
+// });
